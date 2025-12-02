2bb5369f565f9a9fcd1cbe0d088afc373e3298f0
iswahyudi030303 <124139465+iswahyudi030303@users.noreply.github.com>
2025-12-02 22:56:19 +0700

    Add files via upload

---
+++ env.js
@@ -0,0 +1,32 @@
+# =============================================
+# TELEGRAM CONFIG (YANG DIPAKAI)
+# =============================================
+TELEGRAM_BOT_TOKEN=8480624318:AAF3xPQ_AlU60nZRxEz6hohM-rr37Y0CA80
+
+# =============================================
+# REMINDER SETTINGS (YANG DIPAKAI)
+# =============================================
+BAK_REMINDER_DAYS=7
+PKS_REMINDER_DAYS=30
+
+# =============================================
+# PIC TELEGRAM CHAT ID (YANG DIPAKAI)
+# =============================================
+AFRIZAL_TELE=-1003312591351
+DONNY_TELE=-1003450943767
+LINGGA_TELE=-1003489329494
+SPV_TELE=-1003376769813
+RM_TELE=-1003282547792
+
+# =============================================
+# WHATSAPP NUMBERS (format: +6281234567890 atau 081234567890)
+# =============================================
+
+# PIC LANDLEASE - WHATSAPP NUMBERS
+AFRIZAL_WA=+6281213025280
+DONNY_WA=+6281314920003
+LINGGA_WA=+6281298588467
+
+# MANAGEMENT - WHATSAPP NUMBERS
+SPV_WA=+6285156900977
+RM_WA=+62895800088833
